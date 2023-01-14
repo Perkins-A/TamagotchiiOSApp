e0b8e0b9dcf5363028d6fea9c86b2e35990d66cf
Alex Perkins <perkins.a@etoncollege.org.uk>
2023-01-14 10:51:32 +0000

    Initial Commit

---
+++ TamagotchiiOSApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TamagotchiiOSApp
+//
+//  Created by Perkins, Alexander (ABH) on 14/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TamagotchiiOSApp/TamagotchiiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiiOSAppApp.swift
+//  TamagotchiiOSApp
+//
+//  Created by Perkins, Alexander (ABH) on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
